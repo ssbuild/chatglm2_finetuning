c8e8e555c3f2c7de722fa47cb19f7706e87b02d7
ssbuild <462304@qq.com>
2023-08-11 20:55:46 +0800

    update
    Signed-off-by: ssbuild <462304@qq.com>

--- convert_lora_to_peft.py
+++ convert_lora_to_peft.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2023/3/9 15:29
+# @Time:  20:50
+# @Author: tk
+# @File：convert_lora_to_peft
+
 import os
 import torch
-from deep_training.data_helper import ModelArguments, DataArguments
+from deep_training.data_helper import ModelArguments
 from transformers import HfArgumentParser
-from data_utils import train_info_args, NN_DataHelper,global_args
-from aigc_zoo.model_zoo.chatglm2.llm_model import MyTransformer,ChatGLMTokenizer,setup_model_profile, ChatGLMConfig,LoraArguments
-
+from data_utils import train_info_args, NN_DataHelper, global_args
+from aigc_zoo.model_zoo.chatglm2.llm_model import MyTransformer, ChatGLMTokenizer, setup_model_profile, ChatGLMConfig, \
+    LoraArguments,LoraModel
 
 if __name__ == '__main__':
     train_info_args['seed'] = None
     parser = HfArgumentParser((ModelArguments,))
-    (model_args,) = parser.parse_dict(train_info_args,allow_extra_keys=True)
+    (model_args, ) = parser.parse_dict(train_info_args, allow_extra_keys=True)
     setup_model_profile()
-    dataHelper = NN_DataHelper(model_args)
+    dataHelper = NN_DataHelper(model_args,)
     tokenizer: ChatGLMTokenizer
     tokenizer, _, _, _ = dataHelper.load_tokenizer_and_config(
         tokenizer_class_name=ChatGLMTokenizer, config_class_name=ChatGLMConfig)
 
     ckpt_dir = './best_ckpt/last'
     config = ChatGLMConfig.from_pretrained(ckpt_dir)
-    
+
     lora_args = LoraArguments.from_pretrained(ckpt_dir)
 
     assert lora_args.inference_mode == True and config.pre_seq_len is None
@@ -37,28 +40,13 @@ if __name__ == '__main__':
                              # device_map = {"":0} # 第一块卡
                              )
     # 加载lora权重
-    pl_model.load_sft_weight(ckpt_dir)
+    pl_model.load_sft_weight(ckpt_dir,is_trainable=True)
 
     pl_model.eval().half().cuda()
 
-    enable_merge_weight = False
-    if enable_merge_weight:
-        # 合并lora 权重 保存
-        pl_model.save_sft_weight(os.path.join(ckpt_dir,'pytorch_model_merge.bin'),merge_lora_weight=True)
+    lora_model: LoraModel = pl_model.backbone # noqa
 
-    else:
-        model = pl_model.get_llm_model()
-
-        text_list = [
-            "写一个诗歌，关于冬天",
-            "晚上睡不着应该怎么办",
-        ]
-        for input in text_list:
-            response, history = model.chat(tokenizer, input, history=[], max_length=2048,
-                                           eos_token_id=config.eos_token_id,
-                                           do_sample=True, top_p=0.7, temperature=0.95, )
-            print("input", input)
-            print("response", response)
+    lora_model.save_pretrained('./new_lora')
 
 
 
