1c6125d9640d42f103fc6c2abf41b85430b910d0
ssbuild <462304@qq.cn>
2023-08-14 10:49:31 +0800

    peft
    Signed-off-by: ssbuild <462304@qq.cn>

--- convert_lora_to_peft.py
+++ convert_lora_to_peft.py
@@ -2,7 +2,7 @@
 # @Time:  20:50
 # @Author: tk
 # @File：convert_lora_to_peft
-
+import gc
 import os
 import torch
 from deep_training.data_helper import ModelArguments
@@ -11,12 +11,14 @@ from data_utils import train_info_args, NN_DataHelper, global_args
 from aigc_zoo.model_zoo.chatglm2.llm_model import MyTransformer, ChatGLMTokenizer, setup_model_profile, ChatGLMConfig, \
     LoraArguments,LoraModel
 
-if __name__ == '__main__':
+
+
+def convert_to_peft(output_dir = './peft_lora'):
     train_info_args['seed'] = None
     parser = HfArgumentParser((ModelArguments,))
-    (model_args, ) = parser.parse_dict(train_info_args, allow_extra_keys=True)
+    (model_args,) = parser.parse_dict(train_info_args, allow_extra_keys=True)
     setup_model_profile()
-    dataHelper = NN_DataHelper(model_args,)
+    dataHelper = NN_DataHelper(model_args, )
     tokenizer: ChatGLMTokenizer
     tokenizer, _, _, _ = dataHelper.load_tokenizer_and_config(
         tokenizer_class_name=ChatGLMTokenizer, config_class_name=ChatGLMConfig)
@@ -26,9 +28,8 @@ if __name__ == '__main__':
 
     lora_args = LoraArguments.from_pretrained(ckpt_dir)
 
-    #非推理模式
+    # 非推理模式
     lora_args.inference_mode = False
-    lora_args.merge_weights = False
 
     # new_num_tokens = config.vocab_size
     # if config.task_specific_params is not None and config.task_specific_params.get('vocab_size', None) is not None:
@@ -42,13 +43,75 @@ if __name__ == '__main__':
                              # device_map = {"":0} # 第一块卡
                              )
     # 加载lora权重
-    pl_model.load_sft_weight(ckpt_dir,is_trainable=True)
+    pl_model.load_sft_weight(ckpt_dir, is_trainable=True)
 
     pl_model.eval().half().cuda()
 
-    lora_model: LoraModel = pl_model.backbone # noqa
+    lora_model: LoraModel = pl_model.backbone  # noqa
 
-    lora_model.save_pretrained('./new_lora')
+    lora_model.save_pretrained(output_dir)
+
+
+    del pl_model
+    gc.collect()
+
+    return tokenizer,config,lora_args
 
 
 
+def get_base_model():
+    train_info_args['seed'] = None
+    parser = HfArgumentParser((ModelArguments,))
+    (model_args,) = parser.parse_dict(train_info_args, allow_extra_keys=True)
+    setup_model_profile()
+    dataHelper = NN_DataHelper(model_args, )
+    tokenizer: ChatGLMTokenizer
+    tokenizer, _, _, _ = dataHelper.load_tokenizer_and_config(
+        tokenizer_class_name=ChatGLMTokenizer, config_class_name=ChatGLMConfig)
+
+    ckpt_dir = './best_ckpt/last'
+    config = ChatGLMConfig.from_pretrained(ckpt_dir)
+
+
+    pl_model = MyTransformer(config=config, model_args=model_args, torch_dtype=torch.float16, )
+
+
+    return pl_model.get_llm_model()
+
+if __name__ == '__main__':
+    tokenizer,config,lora_args = convert_to_peft('./peft_lora')
+
+    from transformers import AutoModelForCausalLM
+    from peft import get_peft_config, get_peft_model, LoraConfig, TaskType
+
+    model_name_or_path = "bigscience/mt0-large"
+    tokenizer_name_or_path = "bigscience/mt0-large"
+
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=True,
+        r=lora_args.r,
+        lora_alpha=lora_args.lora_alpha,
+        lora_dropout=lora_args.lora_dropout,
+        bias=lora_args.bias
+    )
+
+    # model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+    # model = get_peft_model(model, peft_config)
+    # model.print_trainable_parameters()
+
+    model = get_base_model()
+
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
+
+    text_list = [
+        "写一个诗歌，关于冬天",
+        "晚上睡不着应该怎么办",
+    ]
+    for input in text_list:
+        response, history = model.chat(tokenizer, input, history=[], max_length=2048,
+                                       eos_token_id=config.eos_token_id,
+                                       do_sample=True, top_p=0.7, temperature=0.95, )
+        print("input", input)
+        print("response", response)
